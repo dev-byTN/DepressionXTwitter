856136f605a49b8c46dd3b66c73f740d60cb7312
tristan <tristangn77@gmail.com>
2026-01-11 22:54:31 +0100

    fixhing database file routers

--- app/database/database.py
+++ app/database/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 from database.base import Base
 from models.tweet import Tweet
 from dotenv import load_dotenv
@@ -11,22 +12,20 @@ url = os.getenv("DATABASE_URL")
 
 engine = create_engine(url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = SessionLocal()
 
 def get_tweets():
-    session = SessionLocal()
     
     try:
         fetch = readJsonFile()
         listOfTweets = getRelevantData(fetch)
+        session.add_all(listOfTweets)
+        session.commit()
+        print("Tweets saved in the database.")
         
-        for i in listOfTweets:
-            
-            session.add(i)
-            session.commit()
-        print("Tweets saved in the database")
-        
-    finally:
-        session.close()
+    except SQLAlchemyError as e:
+        session.rollback()
+        print("Error creating the database.", e)    
         
         
 def create_database():
@@ -37,7 +36,6 @@ def create_database():
     
 def show_tweets():
     
-    session = SessionLocal()
     try:
         tweet = session.query(Tweet).all()
         
@@ -45,5 +43,42 @@ def show_tweets():
             print( f" {i.id}, {i.username}, {i.tweet}, {i.url}, {i.date}, {i.depressionType}, \
                     {i.createdAt}, {i.followers}, {i.following}, {i.photo}" )
             
-    finally:
-        session.close()
+    except SQLAlchemyError as e:
+        session.rollback()
+        print("Error accessing the databse.", e)
+        
+        
+def add_record(nb_user):
+    
+    try:
+        for i in range(nb_user):
+            username = str(input("Enter a username: "))
+            tweet = str(input("Enter a tweet: "))
+            url = str(input("Enter the url: "))
+            date = input("Enter the date of the tweet: ")
+            type = str(input("Enter the type of depression: "))
+            creation = input("Enter the date of creation of the user: ")
+            nb_followers = int(input("Enter the number of followers: "))
+            nb_following = int(input("Enter the number of following people: "))
+            picture = str(input("Enter the link to the profile picture: "))
+        
+            record = Tweet(username, tweet, url, date, type, creation, nb_followers, nb_following, picture)        
+            session.add(record)
+            
+        session.commit()
+        
+    except SQLAlchemyError as e:
+        session.rollback()
+        print("Error adding user(s) into the database", e)
+        
+        
+
+def delete_record(id):
+    
+    try:
+        session.query(Tweet).filter(Tweet.id == id).delete()
+        session.commit()
+        
+    except SQLAlchemyError as e:
+        session.rollback()
+        print("No user with that id in the database", e)
--- app/test.py
+++ app/test.py
@@ -1,7 +1,6 @@
 from database.database import *
 from database.base import Base
 
-
 if __name__ == "__main__":
     
     print("1- Create a dabase.")
@@ -9,17 +8,32 @@ if __name__ == "__main__":
     print("3- Add a record.")
     print("4- Delete a record")
     print("5- Show database.")
+    print("6- Quit.")
     option = int(input("Choose an option: "))
-    print("\n")
     
-    match(option) :
-        
-        case 1:
-            print("Database creation")
-            Base.metadata.create_all(bind=engine)
+    while(True):
+        print("\n")
+        match(option):
             
-        case 2:
-            get_tweets()
+            case 1:
+                print("Database creation")
+                Base.metadata.create_all(bind=engine)
+                
+            case 2:
+                get_tweets()
 
-        case 5:
-            show_tweets
+            case 3:
+                nb_user = int(input("How many users fo you want to add in the database: "))
+                add_record(nb_user)
+            
+            case 4:
+                id = int(input("Enter the id of the user you want to delete: "))
+                delete_record(id)
+                
+            case 5:
+                show_tweets()
+                
+            case 6:
+                session.close()
+                break
+            
